33e7aed986e8ece0ca1c01c1007d71a3f40fc8b1
Tyler Stamm <tyler.stamm1313@gmail.com>
2026-05-08 20:18:04 -0400

    incremental models working, 36 rows, 33 test passing

--- attendance_dbt/models/staging/stg_attendance.sql
+++ attendance_dbt/models/staging/stg_attendance.sql
@@ -1,18 +1,32 @@
+{{    config(
+        materialized='incremental',
+        incremental_strategy='insert_overwrite',
+	partitioned_by=['attendance_date']
+    )
+}}
+
 with source as (
 
     select * from {{ source('attendance_raw', 'raw_attendance') }}
+
+    {% if is_incremental() %}
+        where cast(date as date) > (
+            select max(attendance_date)
+            from {{ this }}
+        )
+    {% endif %}
 
 ),
 
 renamed as (
 
     select
-        cast(student_id as varchar)   as student_id,
-        cast(school_id  as varchar)   as school_id,
-        cast(date       as date)      as attendance_date,
-        coalesce(cast(present as integer), 0) as is_present,
-        coalesce(cast(enrolled as integer), 1) as is_enrolled
-
+        cast(student_id as varchar)             as student_id,
+        cast(school_id  as varchar)             as school_id,
+        coalesce(cast(present  as integer), 0)  as is_present,
+        coalesce(cast(enrolled as integer), 1)  as is_enrolled,
+	cast(date	as date)		as attendance_date
+	
     from source
 
 )
